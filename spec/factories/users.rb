d855637768e09fa5c1cde50ee80e6d3d2a17e65b
Avene <juc-y@live.jp>
2014-11-08 23:29:48 +0900

    add user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email) {|n| "kent.#{n}.beck@example.com"}
+    name "kent beck"
+    password "good_password"
+    password_confirmation "good_password"
   end
 end
